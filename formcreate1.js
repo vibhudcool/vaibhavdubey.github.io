1d4ce3f895dae67565817ccd81887470a7dcc234
Vaibhav Dubey <vibhud8923@gmail.com>
2026-03-11 10:15:14 +0530

    Add files via upload

---
+++ formcreate1.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const exp = require('express');
+
+const obj = new exp();
+
+obj.get('/form',(req,res)=>{
+        res.sendFile(__dirname+'/formcreate.html');
+
+})
+
+obj.get('/access_data',(req,res)=>{
+        const student ={
+                Name: req.query.name,
+                Reg_id: req.query.regid,
+                Password: req.query.password,
+                University: req.query.university,
+                Gender: req.query.gender
+        }
+        const data = JSON.stringify(student,null,2) + "\n";
+        
+        fs.appendFile("Student.txt",data,(err)=>{
+                if(err){
+                        throw err;
+                }
+                res.send("Data Saved Successfully");
+        })
+});
+
+obj.listen(3000,()=>{
+        console.log("Server is running");
+})
